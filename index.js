22b03265f3682cbc8c3f3639c9de36b2400dc2ff
stephmen <stephmen@gmail.com>
2018-06-17 10:51:28 -0700

    Ouch

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 
 const express = require('express');
 const raspividStream = require('raspivid-stream');
+
 const app = express();
 const wss = require('express-ws')(app);
 
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(`${__dirname  }/index.html`));
+// app.use('/css',express.static(path.join(__dirname, + 'public/stylesheets')));
 
 app.ws('/video-stream', (ws, req) => {
-    console.log('Client connected');
-    ws.send(JSON.stringify({
-      action: 'init',
-      width: '640',
-      height: '480'
-    }));
+  console.log('Client connected');
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '640',
+    height: '480',
+  }));
 
-    var videoStream = raspividStream({ width: 640, height: 480, rotation: 180 });
-    videoStream.on('data', (data) => {
-        ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
-    });
+  const videoStream = raspividStream({ width: 640, height: 480, rotation: 180 });
+  videoStream.on('data', (data) => {
+    ws.send(data, { binary: true }, (error) => { if (error) console.error(error); });
+  });
 
-    ws.on('close', () => {
-        console.log('Client left');
-        videoStream.removeAllListeners('data');
-    });
+  ws.on('close', () => {
+    console.log('Client left');
+    videoStream.removeAllListeners('data');
+  });
 });
 
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.error(err);
   next(err);
-})
+});
 
 app.listen(80, () => console.log('Server started on 80'));
