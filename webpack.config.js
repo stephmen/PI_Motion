c9b19c94a01e0051966c240b03ef375e1cc99c3e
stephmen <stephmen@gmail.com>
2018-07-17 20:11:29 -0400

    Webpack Rebuilt

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+const webpack = require('webpack');
 var nodeExternals = require('webpack-node-externals');
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
