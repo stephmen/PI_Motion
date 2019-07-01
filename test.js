4fdd7a66e146b544000db1e9e7fe78d55a5a65ab
stephmen <stephmen@gmail.com>
2019-07-01 10:30:42 -0400

    function streamToVideo()

---
+++ test.js
@@ -0,0 +1,12 @@
+      ////////
+      // const stream = canvas.captureStream();
+      // video.srcObject = stream;
+      // video.play();
+        ////////
+
+      function streamToVideo() {
+        canvas.captureStream()( stream =>{
+          video.srcObject = stream
+          video.play()}
+        )
+      }
