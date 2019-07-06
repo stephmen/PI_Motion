1de7023bed585c2f74154deb0be90025056c0021
stephmen <stephmen@gmail.com>
2019-07-06 17:03:04 -0400

    raspivid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const raspividStream = require('raspivid-stream');
 const fs = require('fs');
 const app = express();
-const wss = require('express-ws')(app);
+const ws = require('express-ws')(app);
 const path = require('path');
 const spawn = require('child_process').spawn;
 
@@ -19,13 +19,13 @@ app.ws('/video-stream', (ws, req) => {
     height: '480',
   }));
 
-  const videoStream = raspividStream({
-    width: 640,
-    height: 480,
-    rotation: 180
-  });
+  // const videoStream = raspividStream({
+  //   width: 640,
+  //   height: 480,
+  //   rotation: 180
+  // });
 
-  //var videoStream = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '999999999', '-fps', '20', '-b', '5000000', '-o', '-']);
+  var videoStream = spawn('/opt/vc/bin/raspivid', ['-hf', '-w', '1280', '-h', '1024', '-t', '999999999', '-fps', '20', '-b', '5000000', '-o', '-']);
 
 
   videoStream.on('data', (data) => {
